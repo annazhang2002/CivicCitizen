7b1f14ce563bcf738851006a3d7a6a818d8682da
Anna Zhang <annayzhang@fb.com>
2020-07-13 14:25:40 -0700

    Show list of elections on recycler view

--- app/src/main/java/com/example/votingapp/fragments/ElectionFragment.java
+++ app/src/main/java/com/example/votingapp/fragments/ElectionFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -15,8 +17,12 @@ import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.RequestParams;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.votingapp.BuildConfig;
+import com.example.votingapp.adapters.ElectionsAdapter;
 import com.example.votingapp.models.Election;
 import com.example.votingapp.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +34,8 @@ public class ElectionFragment extends Fragment {
     private static final String TAG = "ElectionFragment";
     List<Election> elections;
     AsyncHttpClient client;
+    RecyclerView rvElections;
+    ElectionsAdapter adapter;
 
     private String apiKey = BuildConfig.GOOGLE_API_KEY;
 
@@ -53,20 +61,30 @@ public class ElectionFragment extends Fragment {
 
         client = new AsyncHttpClient();
         elections = new ArrayList<>();
-
+        rvElections = view.findViewById(R.id.rvElections);
+        adapter = new ElectionsAdapter(getContext(), elections);
+        rvElections.setAdapter(adapter);
+        rvElections.setLayoutManager(new LinearLayoutManager(getContext()));
         getElections();
 
     }
 
     public void getElections() {
         RequestParams params = new RequestParams();
-        params.put("key", "5");
 
+        Log.i(TAG, "Network call url: " + Election.ELECTION_QUERY + "?key=" + apiKey);
         client.get(Election.ELECTION_QUERY + "?key=" + apiKey, params, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 // Access a JSON array response with `json.jsonArray`
-                Log.d(TAG, "onSucces to getElections: " + json.jsonObject.toString());
+                try {
+                    JSONArray array = json.jsonObject.getJSONArray("elections");
+                    Log.d(TAG, "onSuccess to getElections: " + array.toString());
+                    elections.addAll(Election.fromJsonArray(array));
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
